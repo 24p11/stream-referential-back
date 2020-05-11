a78294a753e4367bf3e05cc3649b07d3bcda6acc
Elbaz Michael <elbazmichael92@gmail.com>
2020-05-12 00:53:40 +0200

    Use camel assertion
    https://issues.apache.org/jira/browse/CAMEL-15037

--- src/test/java/fr/aphp/referential/load/processor/InputDirectoryRouteProcessorTest.java
+++ src/test/java/fr/aphp/referential/load/processor/InputDirectoryRouteProcessorTest.java
@@ -3,18 +3,14 @@ package fr.aphp.referential.load.processor;
 import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Map;
 
 import org.apache.camel.CamelExecutionException;
-import org.apache.camel.Exchange;
-import org.apache.camel.Message;
 import org.apache.camel.RoutesBuilder;
 import org.apache.camel.builder.RouteBuilder;
 import org.junit.Test;
 import org.springframework.util.Assert;
 
 import fr.aphp.referential.load.route.BaseRouteTest;
-import io.vavr.control.Try;
 
 import static fr.aphp.referential.load.util.CamelUtils.FORMAT;
 import static fr.aphp.referential.load.util.CamelUtils.VALIDITY_DATE;
@@ -35,26 +31,17 @@ public class InputDirectoryRouteProcessorTest extends BaseRouteTest {
     }
 
     @Test
-    public void testValidFileExtension() throws InterruptedException {
+    public void testValidFileExtension() throws InterruptedException, ParseException {
+        // Expected
+        out.expectedHeaderReceived(FORMAT, "F001");
+        out.expectedHeaderReceived(VALIDITY_DATE, new SimpleDateFormat("yyyyMMdd").parse("20200401"));
+        out.expectedMessageCount(1);
+
         // When
         in.sendBody(file("LIBCIM10MULTI.TXT.f001_20200401"));
 
-        // Expected
-        // Reuse camel assertions (expectedHeaderReceived) when it will be possible https://issues.apache.org/jira/browse/CAMEL-15037
-        out.expectedMessageCount(1);
-
         // Then
         assertMockEndpointsSatisfied();
-
-        out.getExchanges().stream()
-                .map(Exchange::getIn)
-                .map(Message::getHeaders)
-                .forEach(headers -> Try.run(() -> asserts(headers)).get());
-    }
-
-    private void asserts(Map<String, Object> headers) throws ParseException {
-        assertEquals("F001", headers.get(FORMAT));
-        assertEquals(new SimpleDateFormat("yyyyMMdd").parse("20200401"), headers.get(VALIDITY_DATE));
     }
 
     @Test
