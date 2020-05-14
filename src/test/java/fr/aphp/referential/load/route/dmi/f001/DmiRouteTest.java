ca6f8dafbd8327ea55927a6b19ab802e2a89c080
Elbaz Michael <elbazmichael92@gmail.com>
2020-05-14 22:49:18 +0200

    Polish

--- src/test/java/fr/aphp/referential/load/route/dmi/f001/DmiRouteTest.java
+++ src/test/java/fr/aphp/referential/load/route/dmi/f001/DmiRouteTest.java
@@ -10,7 +10,6 @@ import fr.aphp.referential.load.message.dmi.f001.DmiMessage;
 import fr.aphp.referential.load.route.BaseRouteTest;
 
 public class DmiRouteTest extends BaseRouteTest {
-
     @Override
     protected RoutesBuilder createRouteBuilder() throws Exception {
         String fileEndpoint = resourceIn("dmi") + "?noop=true&include=historique_liste_lpp_en_sus032020.xls.F001_20200505";
